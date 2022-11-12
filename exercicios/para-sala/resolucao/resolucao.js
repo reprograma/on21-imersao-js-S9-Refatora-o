b43094be4d5e470c0050de84142b6a99b174d558
rosanaemidio <rosanaemidio42@gmail.com>
2022-11-12 10:51:35 -0300

    feat: adiciona resposta das perguntas

---
+++ exercicios/para-sala/resolucao/resolucao.js
@@ -0,0 +1,5 @@
+1- Por que não refatoramos códigos incorretos? Com por exemplo Abusos de OO?
+2- Se um código se demonstra incompleto, por mais que funcione grande parte, o que devemos fazer?
+3- Ao decorrer da refatoração você se depara com um processo muito complexo que não poderia otimizar, qual a melhorar maneira de prosseguir?
+4- Como diferenciamos refatoração em linguagens back-end e front-end?
+5- Podemos dividir a tarefa de refatorar um código?
