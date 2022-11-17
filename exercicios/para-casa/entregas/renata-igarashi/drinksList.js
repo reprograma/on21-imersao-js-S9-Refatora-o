fbcc1831c655027acfea24dcd45ddfdb1b41f909
Renata Igarashi <renata.sayuri@dasa.com.br>
2022-11-17 19:29:41 -0300

    feat: refactor exSala

---
+++ exercicios/para-casa/entregas/renata-igarashi/drinksList.js
@@ -0,0 +1,24 @@
+const drinksList  = [{
+  id: 1,
+  name: "Soda",
+  price: 3.50,
+  flavor: "Strawberrt",
+  brand: "refriKeylinha",
+},
+{
+  id: 2,
+  name: "Juice",
+  price: 2.50,
+  flavor: "Grape",
+  brand: "suquinhoKeylinha",
+},
+{
+  id: 3,
+  name: "Tea",
+  price: 3.00,
+  flavor: "Mate",
+  brand: "mateKeylinha",
+
+}];
+
+module.exports =  drinksList 
