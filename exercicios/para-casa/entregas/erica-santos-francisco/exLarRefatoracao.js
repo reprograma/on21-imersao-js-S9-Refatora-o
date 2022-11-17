ba7b9ec405c6d872e1e6c005784b0604764b8037
Erica S. Francisco <erykatdb@yahoo.com.br>
2022-11-16 23:31:01 -0300

    feat: 🎸 refactoring second exercice

--- exercicios/para-casa/entregas/erica-santos-francisco/exLarRefatoracao.js
+++
@@ -1,8 +0,0 @@
-/* Vamos refatorar em sala o projeto Exercicio Semana 8 - Resolução professora . Salve o antigo projeto e identifique as mudanças feitas durante o processo de refatoração. */
-
-/*
- - Alteramos as variáveis de var pra let
- - Alteramos os nomes das variáveis
- - Implementamos o código com uma função nova
-
-*/
