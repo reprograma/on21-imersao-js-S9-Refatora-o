d153f1be7000bfa96b0f4464540af4a7873a3f3d
Valenthina Temudo <temudo.valenthina@gmail.com>
2022-11-17 16:26:52 -0300

    enviando exercício de casa com a explicação do que foi feito no arquivo resolucao.md

--- exercicios/para-casa/exLarRefatoracao.js
+++ exercicios/para-casa/exLarRefatoracao.js
@@ -1,1 +1,1 @@
-/* Vamos refatorar em sala o projeto Exercicio Semana 8 - Resolução professora . Salve o antigo projeto e identifique as mudanças feitas durante o processo de refatoração. */
+/* Vamos refatorar em sala o projeto Exercício Semana 8 - Resolução professora . Salve o antigo projeto e identifique as mudanças feitas durante o processo de refatoração. */
