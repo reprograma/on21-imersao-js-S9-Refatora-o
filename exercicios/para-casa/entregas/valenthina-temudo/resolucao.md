1cba6a531883821774ee2def6b89ab8ee570b931
Valenthina Temudo <temudo.valenthina@gmail.com>
2022-11-17 16:29:09 -0300

    enviando exercicio

---
+++ exercicios/para-casa/entregas/valenthina-temudo/resolucao.md
@@ -0,0 +1,20 @@
+<h1 align="center"> Exercício de Casa 🏠</h1>
+
+## Refatoração do código 🔨
+
+### Resolução:
+
+- Troca das variáveis 'var' por 'let';
+- Mudança de português para inglês;
+- Retirei o comentário da atividade que a professora deixou;
+- Padronizei e fiz a indentação com o prettier
+
+---
+
+### Para vê o código antes da refatoração é só ir em:
+
+- exercicios/para-sala/exLar.js
+
+### Para vê o código depois da refatoração é só ir em:
+
+- exercicios/para-casa/exLarRefatoracao.js
--- exercicios/para-casa/exLarRefatoracao.js
+++ exercicios/para-casa/exLarRefatoracao.js
@@ -1,1 +1,60 @@
-/* Vamos refatorar em sala o projeto Exercício Semana 8 - Resolução professora . Salve o antigo projeto e identifique as mudanças feitas durante o processo de refatoração. */
+let menu = [
+  {
+    id: 1,
+    name: 'Soda',
+    price: 3.5,
+    flavor: 'Strawberry',
+    brand: 'SodaKeylinha',
+  },
+  {
+    id: 2,
+    name: 'Juice',
+    price: 2.5,
+    flavor: 'Grape',
+    brand: 'JuiceKeylinha',
+  },
+  {
+    id: 3,
+    name: 'Tea',
+    price: 3.0,
+    flavor: 'Herb Tea',
+    brand: 'TeaKeylinha',
+  },
+];
+function drinkMachine() {
+  let choiceDrink = Number(
+    prompt(`Choice the number you want information on:
+  1- Soda
+  2- Juice
+  3- Tea`)
+  );
+
+  switch (choiceDrink) {
+    case 1:
+      alert(`
+      Name: ${menu[0].name}
+      Price: ${menu[0].price}
+      Flavor: ${menu[0].flavor}
+      Brand: ${menu[0].brand}`);
+      break;
+    case 2:
+      alert(`
+      Name: ${menu[1].name}
+      Price: ${menu[1].price}
+      Flavor: ${menu[1].flavor}
+      Brand: ${menu[1].brand}`);
+      break;
+    case 3:
+      alert(`
+      Name: ${menu[2].name}
+      Price: ${menu[2].price}
+      Flavor: ${menu[2].flavor}
+      Brand: ${menu[2].brand}`);
+      break;
+  }
+}
+drinkMachine();
+let backToMenu = confirm('Do you want to return to the menu?');
+while (backToMenu == true) {
+  drinkMachine();
+}
