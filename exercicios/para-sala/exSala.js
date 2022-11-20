14f337348dbd08d3bb672d4c862f98641fa6d2d0
Eliane Almeida <elianevia@gmail.com>
2022-11-20 19:09:35 -0300

    atividade

--- exercicios/para-sala/exSala.js
+++ exercicios/para-sala/exSala.js
@@ -1,56 +1,64 @@
 //Exercicio para a Sala - Refatore o código abaixo:
-var menu = [{
+const prompt = require("prompt-sync")();
+
+var menu = [
+  {
     id: 1,
     nome: "Refrigerante",
-    preco: 3.50,
+    preco: 3.5,
     sabor: "Morango",
     marca: "RefriKeylinha",
     bio: `Nome: ${menu[0].nome}
     Preço: ${menu[0].preco}
     Sabor: ${menu[0].sabor}
-    Marca: ${menu[0].marca}`
-},
-{
+    Marca: ${menu[0].marca}`,
+  },
+  {
     id: 2,
     nome: "Suco",
-    preco: 2.50,
+    preco: 2.5,
     sabor: "Uva",
     marca: "SuquinhoKeylinha",
     bio: `Nome: ${menu[1].nome}
     Preço: ${menu[1].preco}
     Sabor: ${menu[1].sabor}
-    Marca: ${menu[1].marca}`
-},
-{
+    Marca: ${menu[1].marca}`,
+  },
+  {
     id: 3,
     nome: "Chá",
-    preco: 3.00,
+    preco: 3.0,
     sabor: "Mate",
     marca: "MateKeylinha",
     bio: `Nome: ${menu[2].nome}
     Preço: ${menu[2].preco}
     Sabor: ${menu[2].sabor}
-    Marca: ${menu[2].marca}`
-}];
+    Marca: ${menu[2].marca}`,
+  },
+];
 
 function ofertaMenu() {
-    var escolha = Number(prompt(`Informe o numero que deseja informações:
+  var escolha = Number(
+    prompt(`Informe o numero que deseja informações:
     1- Refrigerante
     2- Suco
-    3- Chá`));
+    3- Chá`)
+  );
 
-    switch (escolha) {
-        case 1: alert(menu[0].bio);
-            break;
-        case 2: alert(menu[1].bio);
-            break;
-        case 3: alert(menu[2].bio);
-            break;
-
-    }
+  switch (escolha) {
+    case 1:
+      alert(menu[0].bio);
+      break;
+    case 2:
+      alert(menu[1].bio);
+      break;
+    case 3:
+      alert(menu[2].bio);
+      break;
+  }
 }
 ofertaMenu();
 var retornaMenu = confirm("Deseja retornar ao menu?");
-    while(retornaMenu == true){
-        ofertaMenu();
+while (retornaMenu == true) {
+  ofertaMenu();
 }
