bb02e558a078428d23bbaf0acdb78282c94a29e3
Raissa Barata <raissa.barata@ifood.com.br>
2022-11-21 14:49:14 -0300

    adiciona resolução de exercicio casa

--- exercicios/para-sala/index.js
+++ exercicios/para-sala/index.js
@@ -1,1 +1,1 @@
-//exercicios feitos pela profa aqui
+'Por que não refatoramos códigos incorretos? Com por exemplo Abusos de OO? Se um código se demonstra incompleto, por mais que funcione grande parte, o que devemos fazer? Ao decorrer da refatoração você se depara com um processo muito complexo que não poderia otimizar, qual a melhorar maneira de prosseguir? Como diferenciamos refatoração em linguagens back-end e front-end? Podemos dividir a tarefa de refatorar um código? '
