26080fb7bf11fe1eb79d8b68c440c4df176a5b56
karumundo <karumundo@gmail.com>
2022-12-08 21:06:03 -0300

    Finalização da refatoração

--- exercicios/para-casa/entregas/caroline-silva-lima/machineItems.js
+++ exercicios/para-casa/entregas/caroline-silva-lima/machineItems.js
@@ -1,50 +1,40 @@
 const machineItems = [
-    {
-        'name':'coca-cola',
-        'netWeight': 350,
-        'price': 5,
-        'amount': 10
-    },
-    {
-         'name':'natural one laranja',
-         'netWeight': 350,
-         'price': 6,
-         'amount': 6
-     },  
-    {
-         'name':'guarana',
-         'netWeight': 350,
-         'price': 5,
-         'amount': 9
-     },
-     {
-         'name':'ice tea',
-         'netWeight': 350,
-         'price': 5,
-         'amount': 6
-     },
-     {
-         'name':'natural one uva',
-         'netWeight': 350,
-         'price': 6,
-         'amount': 5
-     },
-     {
-         'name':'natural one maçã',
-         'netWeight': 350,
-         'price': 6,
-         'amount': 6
-     },
- ];
-/* function teste (bebida){
-    if (machineItems.map(machineItems => machineItems.name === bebida)){
-        const teste1 = `tudo certo`
-    }
-    else {
-        const teste2 = `turo errado`
-    }
-} 
+  {
+    name: "coca-cola",
+    netWeight: 350,
+    price: 5,
+    amount: 10,
+  },
+  {
+    name: "natural one laranja",
+    netWeight: 350,
+    price: 6,
+    amount: 6,
+  },
+  {
+    name: "guarana",
+    netWeight: 350,
+    price: 5,
+    amount: 9,
+  },
+  {
+    name: "ice tea",
+    netWeight: 350,
+    price: 5,
+    amount: 6,
+  },
+  {
+    name: "natural one uva",
+    netWeight: 350,
+    price: 6,
+    amount: 5,
+  },
+  {
+    name: "natural one maçã",
+    netWeight: 350,
+    price: 6,
+    amount: 6,
+  },
+];
 
- console.log(teste('coca-cola'));*/
-
- module.exports = machineItems;
+module.exports = machineItems;
--- exercicios/para-casa/entregas/caroline-silva-lima/menu.js
+++ exercicios/para-casa/entregas/caroline-silva-lima/menu.js
@@ -1,101 +1,63 @@
+const machineItems = require("./machineItems");
+const readline = require("readline-sync");
+
+function addDrink(drinkName, value) {
+  let menuItem = machineItems.find(
+    (machineItems) => machineItems.name === drinkName
+  );
+  let newAmount = (menuItem.amount += value);
+  return "drinks added successfully";
+};
+
+function printMenu() {
+  const menu = machineItems.map(
+    (machineItems) => `${machineItems.name} , price ${machineItems.price}`
+  );
+  console.table(menu);
+  console.log("These are the drinks available");
+};
+
+const cart = [];
+
+function chooseDrink(){
+  const choose = readline.question("Which drink do you want? ");
+  cart.push(choose);
+  const wantMore = readline.question(`Want some more drink? yes/no `);
+  if (wantMore === 'yes'){
+    return chooseDrink();
+  }
+};
+
+function enter() {
+  printMenu();
+  chooseDrink();
+  console.log(`Purchase summary follows ${cart}. Remove your drink `);
+};
+
+console.log(enter());
+
+/*
+Código antigo
+
 const machineItems = require('./machineItems');
 const readline = require('readline-sync');
 
-function newOffer () {
-    readline.question(`Want some more drink? yes/no`);
+const menu =  machineItems.map(machineItems=> (`${machineItems.name} , price ${machineItems.price} ,`));
+const entrance = readline.question('How about a drink? yes / no ');
+const drinkChoice = readline.question ('❤️ Enter the name of the drink you want ❤️');
+
+
+if (entrance === 'yes'){
+  console.table(menu); 
+  const drinkChoice = readline.question ('❤️ Enter the name of the drink you want ❤️');
+  const answer = machineItems.find(machineItems => machineItems.name === drinkChoice);
+  console.log(answer);
+}
+else{
+  console.table(`These are the drinks available: ${ menu }}`);
 };
 
-function drinkChoice () {
-    readline.question ('❤️ Enter the name of the drink you want ❤️');
-};
+const final = console.log(`Retire sua bebida`);
 
-function final(){
-    return `Thanks`
-};
-
-function itensMenu (){
-    const menu =  machineItems.map(machineItems=> (`${machineItems.name} , price ${machineItems.price}`));
-    console.table(menu);
-    console.log('These are the drinks available')
-};
-
-function option(){
-    const option = readline.question('How about a drink? yes / no ');
-    if (option === 'yes'){
-        // criar laço de repetição com break
-        drinkChoice();
-        newOffer();
-        drinkChoice();
-        ;
-    }
-    else if (drinkChoice === no){
-        const newOption = readline.question ("Are you sure you don't want anything? yes/no");
-    }
-    else{
-        final()
-    };
-};
-
-
-    /*if (newOffer === 'yes'){
-        `${itensMenu()}`
-        const drinkChoice = readline.question ('❤️ Enter the name of the drink you want ❤️');
-        //console.log(drinkChoice);
-        console.log(`Remove your drink`)
-    }
-    else{
-        console.log('I understand. When you want to drink something come back here!');
-    };*/
-
-
-function enter(){
-    itensMenu();
-    option();
-        /*switch (option){
-            case machineItems.map:
-                (requestion())
-            default:
-                console.log('I understand. When you want to drink something come back here!')
-        };
-   (requestion());*/
-   (final())
-};
-
-console.log(enter())
-
-
-/*console.log(entrance); 
-    if (entrance === 'yes'){
-        console.log(drinkChoice);
-        console.log(answer);
-    }
-    else{
-        console.table(`These are the drinks available: ${ menu }}`);
-        console.log(answer)
-    };
-    return `Retire sua bebida`;
-
-/*const entrance = readline.question('How about a drink? yes / no ');
-    if (entrance === 'yes'){
-    console.table(menu); 
-    const drinkChoice = readline.question ('❤️ Enter the name of the drink you want ❤️');
-    const answer = machineItems.find(machineItems => machineItems.name === drinkChoice);
-    console.log(answer);
-    }
-    else{
-    console.table(`These are the drinks available: ${ menu }}`);
-    const answer = machineItems.find(machineItems => machineItems.name === drinkChoice);
-    console.log(answer)
-    };
-
-const answer = machineItems.find(machineItems => machineItems.name === drinkChoice);
-console.log(answer)
-
-/*const drinkChoice = readline.question ('❤️ Enter the name of the drink you want ❤️');
-Conforme rodei a aplicação percebi a desnecessidade dessa const repetir nessa linha. Retirei.
-Também movimentei a consta da linha 6 para a linha 8*/
-
-/*module.exports = drinkChoice;
-Retirei o module.exports, já que esse modulo não estava sendo exportado para nenhum local*/
-// Indentei o código corretamente
-// Organizei o código criando funções e coloquei as consts que são reaproveitadas no início do código.
+export default drinkChoice; 
+*/
